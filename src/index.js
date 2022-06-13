3576be10fc773059c0a3daf64a317bea0f80953e
MarvelousMarcin <01159356@pw.edu.pl>
2022-06-13 21:31:23 +0200

    All files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT | 3000;
+const publicDir = "./public/";
+
+app.use(express.static(publicDir));
+app.set("view engine", "html");
+app.set("views", publicDir);
+app.engine("html", require("hbs").__express);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
