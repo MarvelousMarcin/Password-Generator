f8deb9b1af6be5aa5881cadb3274a3cda5249262
MarvelousMarcin <01159356@pw.edu.pl>
2022-06-15 01:29:52 +0200

    quick fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const publicDir = "./build/";
 
 app.use(express.static(publicDir));
